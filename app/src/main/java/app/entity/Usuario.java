037f46a62c0695091da153188310fa97a39b70d6
Alexander <al_emeric@hotmail.com>
2025-04-01 14:49:00 -0300

    atualizando usuario

--- app/src/main/java/app/entity/Usuario.java
+++ app/src/main/java/app/entity/Usuario.java
@@ -1,9 +1,26 @@
 package app.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Usuario {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String userNome;
+
+    private String userSenha;
+
 
 }
--- app/src/main/java/app/service/UsuarioService.java
+++ app/src/main/java/app/service/UsuarioService.java
@@ -1,8 +1,11 @@
 package app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.entity.Usuario;
 import app.repository.UsuarioRepository;
 
 @Service
@@ -10,4 +13,47 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        List<Usuario> lista = this.usuarioRepository.findAll();
+            return lista;
+        
+    }
+
+     public String save (Usuario usuario){
+
+        this.usuarioRepository.save(usuario);
+        return "marca salva com sucesso";
+
+    }
+     public List<Usuario> findByNome(String nome){
+        return this.usuarioRepository.findByNome(nome);
+
+    }
+
+    public String delete(Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            return "Erro: Usuario com ID " + id + " não encontrado.";
+        }
+    
+        usuarioRepository.deleteById(id);
+        return "Usuario removido com sucesso";
+    }
+    
+
+    public String update(Usuario usuario, Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            return "Erro: Marca com ID " + id + " não encontrada.";
+        }
+        
+        usuario.setUserId(id);
+        usuarioRepository.save(usuario);
+        return "Usuario atualizado com sucesso";
+    }
+    
+     public Usuario findById(Long id){
+        Usuario usuario = this.usuarioRepository.findById(id).get();
+        return usuario;
+    }
+
 }
